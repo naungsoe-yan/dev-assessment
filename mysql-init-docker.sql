e5ebd2dbf76c4fe4c87cf5e82bc5ad6450962fc7
Yan Naung Soe <naungsoe.yan@ufinity.com>
2023-11-15 20:51:20 +0800

    Added mysql init script for docker

---
+++ mysql-init-docker.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS `dev-assessment`.`teachers` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `created_date` DATETIME NOT NULL,
+  `updated_date` DATETIME NULL,
+  `email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+
+CREATE TABLE IF NOT EXISTS `dev-assessment`.`students` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `created_date` DATETIME NOT NULL,
+  `updated_date` DATETIME NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `suspended` TINYINT NOT NULL,
+  `suspended_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+
+CREATE TABLE IF NOT EXISTS `dev-assessment`.`registrations` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `created_date` DATETIME NOT NULL,
+  `updated_date` DATETIME NULL,
+  `teacher_id` BIGINT NOT NULL,
+  `student_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_registrations_teachers_id_idx` (`teacher_id` ASC) VISIBLE,
+  INDEX `fk_registrations_students_id_idx` (`student_id` ASC) VISIBLE,
+  CONSTRAINT `fk_registrations_teachers_id`
+    FOREIGN KEY (`teacher_id`)
+    REFERENCES `dev-assessment`.`teachers` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_registrations_students_id`
+    FOREIGN KEY (`student_id`)
+    REFERENCES `dev-assessment`.`students` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
