59ca7744387320314e3bb04b6e1b7a643f7b37be
Yan Naung Soe <naungsoe.yan@ufinity.com>
2023-11-15 23:09:18 +0800

    Updated mysql init script for docker

--- mysql-init-docker.sql
+++ mysql-init-docker.sql
@@ -1,4 +1,41 @@
-CREATE TABLE IF NOT EXISTS `test_table` (
-  `test_field` varchar(255) DEFAULT NULL,
-  `test_field2` int(11) DEFAULT 0
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS registrations;
+DROP TABLE IF EXISTS teachers;
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE teachers (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  created_date DATETIME NOT NULL,
+  updated_date DATETIME NULL,
+  email VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX email_UNIQUE (email ASC) VISIBLE);
+
+CREATE TABLE students (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  created_date DATETIME NOT NULL,
+  updated_date DATETIME NULL,
+  email VARCHAR(45) NOT NULL,
+  suspended TINYINT NOT NULL,
+  suspended_date DATETIME NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX email_UNIQUE (email ASC) VISIBLE);
+
+CREATE TABLE registrations (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  created_date DATETIME NOT NULL,
+  updated_date DATETIME NULL,
+  teacher_id BIGINT NOT NULL,
+  student_id BIGINT NOT NULL,
+  PRIMARY KEY (id),
+  INDEX fk_registrations_teachers_id_idx (teacher_id ASC) VISIBLE,
+  INDEX fk_registrations_students_id_idx (student_id ASC) VISIBLE,
+  CONSTRAINT fk_registrations_teachers_id
+    FOREIGN KEY (teacher_id)
+    REFERENCES teachers (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_registrations_students_id
+    FOREIGN KEY (student_id)
+    REFERENCES students (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
